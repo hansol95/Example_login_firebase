3f9ee902fce5b4926aec72f3afc47fbffb98ed2a
hansol95 <Gksthfbts92@>
2023-02-02 16:46:16 +0900

    Initial commit

---
+++ app/src/main/java/com/example/example_login_firebase/Information.java
@@ -0,0 +1,2 @@
+package com.example.example_login_firebase;public class Information {
+}
---
+++ app/src/main/java/com/example/example_login_firebase/MyPageActivity.java
@@ -0,0 +1,14 @@
+package com.example.example_login_firebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_page);
+    }
+}
